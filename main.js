b450983408e390ff7176e2a0700dd19ff648e1ab
Muhammed Onen <30301543+muhammedonen@users.noreply.github.com>
2022-05-15 14:54:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,26 +4,7 @@ var soon;
 var Buy;
 var Name; 
 
-fetch("https://www.akbank.com/_vti_bin/AkbankServicesSecure/FrontEndServiceSecure.svc/GetExchangeData?_=1649283941521", {
-  "headers": {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-    "cache-control": "max-age=0",
-    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"101\", \"Google Chrome\";v=\"101\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
-  },
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "include"
-})
+fetch(url)
   .then((res) => res.json())
   .then((response) => {
     soon = JSON.parse(response.GetExchangeDataResult);
